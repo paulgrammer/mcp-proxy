c28f4b9f5b4682fb4f85b13a7857f62bd578abcf
paulgrammer <paulmugaya@live.com>
2025-06-06 22:08:21 +0200

    feat: Add initial implementation of Prompt and Resource types

--- prompt.go
+++ prompt.go
@@ -1,1 +1,33 @@
 package proxy
+
+import (
+	"github.com/mark3labs/mcp-go/mcp"
+)
+
+// Prompt represents the final configured prompt
+type Prompt struct{}
+
+// NewPrompt creates a prompt
+func NewPrompt() *Prompt {
+	return &Prompt{}
+}
+
+func (p *Prompt) Prompt() mcp.Prompt {
+	return mcp.NewPrompt(
+		p.Name(),
+		// mcp.WithArgument() TODO: add arguments
+		mcp.WithPromptDescription(p.Description()),
+	)
+}
+
+func (p *Prompt) Name() string {
+	return "Not implemented"
+}
+
+func (p *Prompt) Type() string {
+	return "Not implemented"
+}
+
+func (p *Prompt) Description() string {
+	return "Not implemented"
+}
--- resource.go
+++ resource.go
@@ -1,1 +1,18 @@
 package proxy
+
+import (
+	"context"
+	"errors"
+
+	"github.com/mark3labs/mcp-go/mcp"
+)
+
+type Resource struct{}
+
+func (r *Resource) ResourceTemplate() mcp.ResourceTemplate {
+	panic("not implemented")
+}
+
+func (r *Resource) Handler(ctx context.Context, req mcp.ReadResourceRequest) ([]mcp.ResourceContents, error) {
+	return []mcp.ResourceContents{}, errors.New("not implemented")
+}
